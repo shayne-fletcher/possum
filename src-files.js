bd3091867c7db7217dda8e8bd9eeeb2b42bb5370
shayne-fletcher <shayne-fletcher@users.noreply.github.com>
2024-10-20 21:51:28 +0000

    deploy: 9368c1f0368c6479bcc9b775d1385e5641a875a6

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["possum",["",[["commands",[["model",[],["download.rs","metadata.rs","mod.rs","revisions.rs","search.rs"]]],["mod.rs"]]],["main.rs"]]]\
+]'));
+createSrcSidebar();
