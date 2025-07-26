bea767bf5e00e98e8cc0b00e0e9838639ca717a4
shayne-fletcher <shayne-fletcher@users.noreply.github.com>
2025-07-26 18:28:10 +0000

    deploy: 8224b9b14de810629d2734bb72352e61390fb58b

--- possum/commands/model/revisions/sidebar-items.js
+++ possum/commands/model/revisions/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["build_revisions_url","extract_branch_names","parse_branches_from_json","revisions"]};
+window.SIDEBAR_ITEMS = {"fn":["build_revisions_url","extract_branch_names","revisions"]};
